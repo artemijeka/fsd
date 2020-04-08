c6086e4f0d2194492382a712ddb0eb8e38e30b4f
Artem <artem@varnoff.ru>
2020-04-08 21:45:28 +0400

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import('./libs/jquery.js').then(function(data0) {
-  import('./libs/jquery.nice-select.min.js').then(function(data1) {
-    $(document).ready(function() {
-      $('select.text-field__select').niceSelect();
-    });
-  });
+import './libs/jquery.js';
+import './libs/jquery.nice-select.min.js';
+
+
+$(document).ready(function() {
+  $('select.text-field__select').niceSelect();
 });
