f949ef402aba4dd03dde209e41b314031e64225b
Artem <artem@varnoff.ru>
2020-04-12 13:32:08 +0400

    subscription text field

--- src/js/footer/10.dev.js
+++ src/js/footer/10.dev.js
@@ -4,19 +4,15 @@ $(function() {
   $('select.input__dropdown').niceSelect();
 
 
-  $(".input__masked").inputmask('datetime', {
+  $(".jsMaskedDate").inputmask('datetime', {
     mask: "1.2.y",
     alias: "dd:mm:yyyy",
     placeholder: "ДД.ММ.ГГГГ",
     separator: '.'
   });
-  $(".input__masked").on('keyup', function(){
-    var maskedInput = $('.input__masked').val();
-    console.log( maskedInput );
-  });
 
 
-  $('.input__dropdown_date').datepicker({
+  $('.jsDateDropdown').datepicker({
     dateFormat: "dd.mm.yy"
   });
 
