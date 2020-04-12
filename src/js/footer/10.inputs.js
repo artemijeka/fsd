2e01d287a51bda8126b28c495eb1489c8f58e02b
Artem <artem@varnoff.ru>
2020-04-12 16:34:27 +0400

    start DROPDOWN EXPANDED

---
+++ src/js/footer/10.inputs.js
@@ -0,0 +1,45 @@
+$(function() {
+  
+
+  $('.jsSelect').niceSelect();
+
+
+  $(".jsMaskedDate").inputmask('datetime', {
+    mask: "1.2.y",
+    alias: "dd:mm:yyyy",
+    placeholder: "ДД.ММ.ГГГГ",
+    separator: '.'
+  });
+
+
+  $('.jsDateDropdown').datepicker({
+    dateFormat: "dd.mm.yy"
+  });
+
+
+
+  $('[data="openDropdownList"]').on('click', function(){
+    console.log('test');
+    $(this).toggleClass('expanded');
+  });
+
+
+  $('[data="numberValueMore"]').on('click', function(e){
+    let currentVal = $(this).prev().val();
+    $(this).prev().prev().removeClass('disable');
+    $(this).prev().val(++currentVal);
+  });
+
+  $('[data="numberValueLess"]').on('click', function(e) {
+    let currentVal = Number( $(this).next().val() );
+    console.log(currentVal);
+    if (currentVal === 1) {
+      $(this).addClass('disable');
+    }  
+    if (currentVal > 0) {
+      $(this).next().val(--currentVal);
+    } 
+      
+  });
+
+});
