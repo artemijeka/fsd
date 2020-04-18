c1cfbc85a745ccf57c15e7ddeed3c9a1273c6510
Artem <artem@varnoff.ru>
2020-04-18 23:33:14 +0400

    доперло как в класе сделать в событии обращение к методу

--- src/js/footer/20.dropdown-room.js
+++ src/js/footer/20.dropdown-room.js
@@ -1,65 +1,71 @@
-// $(function(){
+'use strict';
 
 
-  var dropdownRoom = new Dropdown('#dropdown2');
+var objDropdownRoom = new Dropdown('#dropdown2');
 
 
-  function Dropdown(id) {
-    
-    //сам dropdown (input)
-    this.inputDropdown = document.querySelector(id);
+function Dropdown(id) {
 
-    //лист вариантов (ul)
-    this.arInputsDropdownList = this.inputDropdown.nextSibling.nextSibling.nextSibling.children;
 
-    //объект с данными текущего dropdown
-    this.objDataDropdown = {};
+  //сам dropdown (input)
+  this.inputDropdown = document.querySelector(id);
 
-    //метод обновления данных dropdown
-    this.refreshDataDropdown = (name, val) => {
-      this.objDataDropdown[name] = val;
-      console.log(this.objDataDropdown);
-    };
 
-    this.init = function() {
+  //лист вариантов (ul)
+  this.arInputsDropdownList = this.inputDropdown.nextSibling.nextSibling.nextSibling.children;
 
-      //инициализация щелчка на dropdown
-      this.inputDropdown.onclick = function(event) {
-        event.target.classList.toggle('expanded');
+
+  //объект с данными текущего dropdown
+  this.objDataDropdown = {};
+
+
+  //инициализация щелчка на dropdown
+  this.inputDropdown.onclick = function(event) {
+    event.target.classList.toggle('expanded');
+  };
+
+
+  //метод обновления данных dropdown
+  this.refreshDataDropdown = function(name, val) {
+    this.objDataDropdown[name] = val;
+  };
+
+  
+  //инициализация объекта с данными и кликов на + и на -
+  for (let item of this.arInputsDropdownList) {
+    let less = item.childNodes[3].childNodes[1];
+    let value = item.childNodes[3].childNodes[3];
+    let more = item.childNodes[3].childNodes[5];
+    let self = this;
+
+    //инициализация объекта с данными dropdown
+    this.objDataDropdown[value.getAttribute('data-name')] = value.value;
+
+    if (Number(value.value) === 0) {
+      less.classList.add('disable');
+    }
+
+    less.onclick = function() {
+      let currentVal = Number(value.getAttribute('value'));
+      if (currentVal === 1) {
+        less.classList.add('disable');
       }
-
-      //инициализация объекта с данными и кликов на + и на -
-      for (let item of this.arInputsDropdownList) {
-        // console.log( item );
-        let less = item.childNodes[3].childNodes[1];
-        let value = item.childNodes[3].childNodes[3];
-        let more = item.childNodes[3].childNodes[5];
-        if ( Number( value.getAttribute('value') ) === 0 ) {
-          less.classList.add('disable');
-        }
-        less.onclick = function() {
-          let currentVal = Number( value.getAttribute('value') );
-          if ( currentVal === 1 ) {
-            less.classList.add('disable');
-          }
-          if ( currentVal > 0 ) {
-            value.setAttribute('value', --currentVal);
-          }
-          // this.refreshDataDropdown( value.getAttribute('data-name'), currentVal );
-          console.log(currentVal);
-        }
-        more.onclick = function() {
-          let currentVal = Number( value.getAttribute('value') );
-          less.classList.remove('disable');
-          value.setAttribute('value', ++currentVal);
-          console.log(currentVal);
-        }
-        
+      if (currentVal > 0) {
+        value.setAttribute('value', --currentVal);
       }
+      self.refreshDataDropdown(value.getAttribute('data-name'), currentVal);
+      console.log(self.objDataDropdown);
+    }
 
-    };
-    this.init();
-    
+    more.onclick = function() {
+      let currentVal = Number(value.getAttribute('value'));
+      less.classList.remove('disable');
+      value.setAttribute('value', ++currentVal);
+      self.refreshDataDropdown(value.getAttribute('data-name'), currentVal);
+      console.log(self.objDataDropdown);
+    }
   }
 
-// });
+  // console.log(this.objDataDropdown);
+
+}
