5b0ca58d49e15e456ee28258a629d7a0d58a6579
Artem <artem@varnoff.ru>
2020-04-19 16:47:54 +0400

    EXPANDABLE CHECKBOX LIST

---
+++ src/js/footer/30.checkbox-list.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+let checkboxList1 = new CheckboxList('#checkboxList1');
+let checkboxList2 = new CheckboxList('#checkboxList2');
+
+
+function CheckboxList(id) {
+
+
+  //инициализируем объект чтобы потом обращаться к нему внутри событий
+  let self = this;
+
+
+  //инициализация блока в DOM
+  this.checkboxList = document.querySelector(id);
+
+
+  //выбираем все переключатели списка
+  this.checkboxListToggle = this.checkboxList.querySelector('[data-toggle]');
+  // console.log( this.checkboxListToggle );
+  
+  
+  //инициализация щелчка на toggle
+  this.checkboxListToggle.onclick = function() {
+    self.checkboxList.toggleAttribute('data-expanded');
+  }
+  
+
+
+
+  
+
+}
