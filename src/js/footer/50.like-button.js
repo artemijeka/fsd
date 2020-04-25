883717d1158fe281e005d203602825861a84128f
Artem <artem@varnoff.ru>
2020-04-25 19:08:47 +0400

    50.like-button.js

---
+++ src/js/footer/50.like-button.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+let likeButton1 = new LikeButton('#likeButton1');
+let likeButton2 = new LikeButton('#likeButton2');
+
+
+function LikeButton( id ) {
+
+
+  let self = this;
+
+
+  this.likeButton = document.querySelector( id );
+  // this.likeButton.value; 
+  
+  
+  this.changeValue = function() {
+    console.log(self.likeButton.getAttribute('data-like-checked') == 'true');
+    if (self.likeButton.getAttribute('data-like-checked') == 'true') {
+      self.likeButton.value--;
+      self.likeButton.setAttribute('data-like-checked', 'false');
+    } else {
+      self.likeButton.value++;
+      self.likeButton.setAttribute('data-like-checked', 'true');
+    }
+  }
+
+
+  this.likeButton.onclick = this.changeValue;
+  
+
+}
